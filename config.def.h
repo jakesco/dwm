3a01b368470dec95d2c803ebaf75ffdc9ddf2685
Jake Scott <scottj@sou.edu>
2020-06-29 12:31:46 -0700

    update config.def.h

--- config.def.h
+++ config.def.h
@@ -26,10 +26,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     iscentered   isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           1,           -1 },
-	{ "firefox",  NULL,       NULL,       0,            0,           0,           -1 },
-	{ "spotify",  NULL,       NULL,       1 << 8,       0,           0,           -1 },
+	/* class        instance    title       tags mask     iscentered   isfloating   monitor */
+	{ "Gimp",       NULL,       NULL,       0,            0,           1,           -1 },
+	{ "firefox",    NULL,       NULL,       0,            0,           0,           -1 },
+	{ "spotify",    NULL,       NULL,       1 << 8,       0,           0,           -1 },
+	{ "Alacritty",  NULL,       "scratch",  0,            1,           1,           -1 },
 };
 
 /* layout(s) */
